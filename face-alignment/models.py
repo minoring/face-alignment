df8ecccdf34d870718eeaaec61d297d09fb9a603
minoring <minho1350@gmail.com>
2020-01-04 23:23:04 -0800

    Implement model

--- face-alignment/models.py
+++ face-alignment/models.py
@@ -121,10 +121,11 @@ class HourGlass(nn.Module):
     if level > 1:
       self._generate_network(level - 1)
     else:
-      self.add_module('b2_plus_' + str(level), ConvBlock(self.features, self.features))
-    
+      self.add_module('b2_plus_' + str(level),
+                      ConvBlock(self.features, self.features))
+
     self.add_module('b3_' + str(level), ConvBlock(self.features, self.features))
-  
+
   def _forward(self, level, inp):
     # Upper branch
     up1 = inp
@@ -146,6 +147,129 @@ class HourGlass(nn.Module):
     up2 = F.interpolate(low3, scale_factor=2, mode='nearest')
 
     return up1 + up2
-  
+
   def forward(self, x):
     return self._forward(self.depth, x)
+
+
+class FAN(nn.Module):
+
+  def __init__(self, num_modules=1):
+    super(FAN, self).__init__()
+    self.num_modules = num_modules
+
+    # Base part
+    self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3)
+    self.bn1 = nn.BatchNorm2d(64)
+    self.conv2 = ConvBlock(64, 128)
+    self.conv3 = ConvBlock(128, 128)
+    self.conv4 = ConvBlock(128, 256)
+
+    # Stacking part
+    for hg_module in range(self.num_modules):
+      self.add_module('m' + str(hg_module), HourGlass(1, 4, 256))
+      self.add_module('top_m_' + str(hg_module), ConvBlock(256, 256))
+      self.add_module('conv_last' + str(hg_module),
+                      nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0))
+      self.add_module('bn_end' + str(hg_module), nn.BatchNorm2d(256))
+      self.add_module('l' + str(hg_module),
+                      nn.Conv2d(256, 68, kernel_size=1, stride=1, padding=0))
+
+      if hg_module < self.num_modules - 1:
+        self.add_module('bl' + str(hg_module),
+                        nn.Conv2d(256, 256, kernel_size=1, stride=1, padding=0))
+        self.add_module('al' + str(hg_module),
+                        nn.Conv2d(68, 256, kernel_size=1, stride=1, padding=0))
+
+  def forward(self, x):
+    x = F.relu(self.bn1(self.conv1(x)), True)
+    x = F.avg_pool2d(self.conv2(x), 2, stride=2)
+    x = self.conv3(x)
+    x = self.conv4(x)
+
+    previous = x
+
+    outputs = []
+    for i in range(self.num_modules):
+      hg = self._modules['m' + str(i)](previous)
+
+      ll = hg
+      ll = self._modules['top_m_' + str(i)](ll)
+      ll = F.relu(
+          self._modules['bn_end' + str(i)](self._modules['conv_last' +
+                                                         str(i)](ll)), True)
+      # Predict heatmaps
+      tmp_out = self._modules['l' + str(i)](ll)
+      outputs.append(tmp_out)
+
+      if i < self.num_modules - 1:
+        ll = self._modules['bl' + str(i)](ll)
+        tmp_out_ = self._modules['al' + str(i)](tmp_out)
+        previous = previous + ll + tmp_out_
+
+    return outputs
+
+
+class ResNetDepth(nn.Module):
+
+  def __init__(self, block=Bottleneck, layers=[3, 8, 36, 3], num_classes=68):
+    self.inplanes = 68
+    super(ResNetDepth, self).__init__()
+    self.conv1 = nn.Conv2d(3 + 68,
+                           64,
+                           kernel_size=7,
+                           stride=2,
+                           padding=3,
+                           bias=False)
+    self.bn1 = nn.BatchNorm2d(64)
+    self.relu = nn.ReLU(inplace=True)
+    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+    self.layer1 = self._make_layer(block, 64, layers[0])
+    self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+    self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+    self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+    self.avgpool = nn.AvgPool2d(7)
+    self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    for m in self.modules():
+      if isinstance(m, nn.Conv2d):
+        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        m.weight.data.normal_(0, math.sqrt(2./n))
+      elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+
+  def _make_layer(self, block, planes, blocks, stride=1):
+    downsample = None
+    if stride != 1 or self.inplanes != planes * block.expansion:
+      downsample = nn.Sequential(
+          nn.Conv2d(self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=1,
+                    stride=stride,
+                    bias=False), nn.BatchNorm2d(planes * block.expansion))
+
+    layers = []
+    layers.append(block(self.inplanes, planes, stride, downsample))
+    self.inplanes = planes * block.expansion
+    for _ in range(1, blocks):
+      layers.append(block(self.inplanes, planes))
+
+    return nn.Sequential(*layers)
+
+  def forward(self, x):
+    x = self.conv1(x)
+    x = self.bn1(x)
+    x = self.relu(x)
+    x = self.maxpool(x)
+
+    x = self.layer1(x)
+    x = self.layer2(x)
+    x = self.layer3(x)
+    x = self.layer4(x)
+
+    x = self.avgpool(x)
+    x = x.view(x.size(0), -1)
+    x = self.fc(x)
+
+    return x
